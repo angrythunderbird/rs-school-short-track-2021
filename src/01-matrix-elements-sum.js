9d6b1cc57668ec853243c221fd49caf473669e4c
angrythunderbird <headcrab.albert@gmail.com>
2021-04-09 23:42:55 +0300

    refactor: delete excess lines in 01-matrix-elements-sum task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,25 +1,6 @@
-/**
- * Given matrix, a rectangular matrix of integers,
- * just add up all the values that don't appear below a "0".
- *
- * @param {Array<Array>} matrix
- * @return {Number}
- *
- * @example
- * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
- * ]
- *
- * The result should be 9
- */
 function getMatrixElementsSum(matrix) {
-  // throw new Error('Not implemented');
   const lengthMatrix = matrix[0].length;
-
   const flatMatrix = matrix.flat();
-
   let sum = 0;
 
   for (let i = 0; i < flatMatrix.length; i++) {
