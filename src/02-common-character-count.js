e72af1010e76b6193cd1287202c63d798cf6274e
angrythunderbird <headcrab.albert@gmail.com>
2021-04-12 00:21:10 +0300

    feat: add solition for 02-common-xharacter-count

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,20 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const aArr = s1.split('');
+  const bArr = s2.split('');
+  let count = 0;
+
+  aArr.map((element) => {
+    if (bArr.includes(element)) {
+      count++;
+      bArr.splice(bArr.indexOf(element), 1);
+    }
+    return count;
+  });
+
+  return count;
 }
 
 module.exports = getCommonCharacterCount;
